93c9cdac2e613723fa30b244dc3e5ccb91f0b563
t2k <a>
2013-05-26 18:22:46 +0900

    Add sources
    Xaml使いにくいよー

---
+++ VendingMachine/Model/MoneyManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Model {
+    
+    /// <summary>
+    /// Singleton dayo.
+    /// </summary>
+    public class MoneyManager {
+
+        protected static MoneyManager _instance;
+
+
+        /// <summary>
+        /// Singleton dayo.
+        /// </summary>
+        private MoneyManager() { 
+        }
+
+        /// <summary>
+        /// Singleton dayo.
+        /// </summary>
+        /// <returns></returns>
+        public static MoneyManager GetInstance() {
+            if (_instance == null) {
+                _instance = new MoneyManager();
+            }
+            return _instance;
+        }
+
+    }
+}
